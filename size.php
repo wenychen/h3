76f9f698355e07739d78f061cfc4f6f13cd57d4d
wenychen <100336203+wenychen@users.noreply.github.com>
2022-03-28 21:31:00 +0800

    Add files via upload

--- size.php
+++ size.php
@@ -1,20 +1,6 @@
 <?php
       session_start();
       
-      if(isset($_SESSION['login'])){
-            
-            if($_SESSION['login']=='Yes'){
-                  echo '<a href="logout.php">登出系統</a>';
-            }else{
-                  echo '非法進入系統';
-                  exit();
-            }
-      }else{
-                  echo '非法進入系統';
-                  exit();
-      }
-
-
 ?>
 <!DOCTYPE HTML>
 
